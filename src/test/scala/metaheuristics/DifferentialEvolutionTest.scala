ff35f189972e573576e547e6db3e8d8ebb2e8654
wilcoschoneveld <schoneveld.wj@gmail.com>
2015-10-16 20:01:55 +0200

    Changed test style from FunSuite to FlatSpec

--- src/test/scala/metaheuristics/DifferentialEvolutionTest.scala
+++ src/test/scala/metaheuristics/DifferentialEvolutionTest.scala
@@ -1,8 +1,22 @@
 package metaheuristics
 
-import org.scalatest.FunSuite
+import org.scalatest.{Matchers, FlatSpec}
 
-class DifferentialEvolutionTest extends FunSuite {
+class DifferentialEvolutionTest extends FlatSpec with Matchers {
+
+  def parabola(x: Double): Double = x*x + 2*x + 3
+
+  "Parabola" should "have minimum of 2" in {
+    val DE = new DifferentialEvolution(N = 10)
+
+    val initial = DE.populate((-10.0, 10.0))
+
+    val generations = DE.evolve(x => parabola(x.head), initial)
+
+    val best = generations(100).minBy(x => parabola(x.head))
+
+    parabola(best.head) should be (2.0 +- 0.01)
+  }
 
   def ackley(x: List[Double], a: Double = 20, b: Double = 0.2, c: Double = 2 * math.Pi): Double = {
     val c1 = -b * math.sqrt(x.map(v => v * v).sum / x.length)
@@ -12,17 +26,15 @@ class DifferentialEvolutionTest extends FunSuite {
     -a * math.exp(c1) - math.exp(c2) + a + math.exp(1)
   }
 
-  test("The Ackley function should converge") {
-    val DE = new DifferentialEvolution(N = 100)
+  "The Ackley function" should "converge to origin" in {
+    val DE = new DifferentialEvolution(N = 10)
 
-    val bounds = List((-5.0, 5.0), (-5.0, 5.0))
-
-    val initial = DE.populate(bounds)
+    val initial = DE.populate((-5.0, 5.0), (-5.0, 5.0))
 
     val generations = DE.evolve(ackley(_), initial)
 
-    val best = generations(1000).minBy(ackley(_))
+    val best = generations(100).minBy(ackley(_))
 
-    assert(best.map(e => e * e).sum < 0.001)
+    all (best) should be (0.0 +- 0.01)
   }
 }
